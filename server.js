2a0ebfab440a89df8f80b34d7cb182289c9df349
Chada Saiteja <59697966+ChadaSaiteja@users.noreply.github.com>
2024-12-28 23:07:33 +0530

    Adding A Record response

--- server.js
+++ server.js
@@ -1,26 +1,49 @@
-const dgram = require('dgram');
+const dgram = require("dgram");
+const dns_packet = require("dns-packet");
+const server = dgram.createSocket("udp4");
 
-const server = dgram.createSocket('udp4');
-
-server.on('error', (err) => {
-    console.log(`Server error:\n${err.stack}`);
-    server.close();
+server.on("error", (err) => {
+  console.log(`Server error:\n${err.stack}`);
+  server.close();
 });
 
 
-server.on('message', (msg, rinfo) => {
-    console.log(rinfo)
-    console.log(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-    server.send("HHHH", rinfo.port, rinfo.address, (err) => {
-        if (err) {
-            console.log(`Error sending message: ${err}`);
-        }
-    });
+//get list from redis or store details in db
+const domainsList = {
+  "example.com": "0.0.0.1",
+  "google.com": "0.0.0.1",
+};
+
+server.on("message", (msg, rinfo) => {
+  const record = dns_packet.decode(msg);
+  const domain = record.questions[0].name;
+  console.log(record.questions);
+  console.log(rinfo);
+
+  const response = dns_packet.encode({
+    type: "response",
+    id: record.id,
+    questions: record.questions,
+    answers: [
+      {
+        name: domain,
+        type: "A",
+        class: "IN",
+        ttl: 300,
+        data: domainsList[domain] ?? "0.0.0.0",
+      },
+    ],
+  });
+  server.send(response, rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.log(`Error sending message: ${err}`);
+    }
+  });
 });
 
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`Server listening ${address.address}:${address.port}`);
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`Server listening ${address.address}:${address.port}`);
 });
 
-server.bind(53); 
+server.bind(4153);
